bd3e061ee316fbb9cc28b4280ef012db0e9ff9db
bigoldguy <finn.almog@gmail.com>
2025-06-02 22:26:03 +1200

    first function to view all riders

--- 2.8_python.py
+++ 2.8_python.py
@@ -1,1 +1,18 @@
-print("hello")
+import sqlite3
+conn = sqlite3.connect('fantasy.DB')
+cursor = conn.cursor()
+
+def display_all_riders(cursor):
+    cursor.execute("SELECT name, cost FROM riders")
+    riders = cursor.fetchall()
+
+    print("No.  | Name                 | Cost")
+    print("-------------------------------------")
+    for i, (name, cost) in enumerate(riders, start=1):
+        print(f"{str(i).ljust(4)} | {name.ljust(20)} | ${cost}")
+
+
+
+
+
+conn.close()
