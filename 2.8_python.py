54c8d92c1dde6692a88357eacdb18583e3a6233e
bigoldguy <finn.almog@gmail.com>
2025-06-02 22:45:59 +1200

    defined budget and M F ratios

--- 2.8_python.py
+++ 2.8_python.py
@@ -1,3 +1,8 @@
+TEAM_BUDGET = 1500000
+TEAM_SIZE = 6
+MAX_MEN = 4
+MAX_WOMEN = 2
+
 import sqlite3
 conn = sqlite3.connect('fantasy.DB')
 cursor = conn.cursor()
@@ -9,10 +14,10 @@ def display_all_riders(cursor):
     print("No.  | Name                 | Cost")
     print("-------------------------------------")
     for i, (name, cost) in enumerate(riders, start=1):
-        print(f"{str(i).ljust(4)} | {name.ljust(20)} | ${cost}")
+        print(f"{str(i).ljust(4)} | {name.ljust(25)} | ${cost}")
 
 
 
-
+display_all_riders(cursor)
 
 conn.close()
